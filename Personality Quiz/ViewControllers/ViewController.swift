374eb6cc6dc79e12cc215f42ebf052619e08919b
Boris Z <Boris Z>
2022-07-04 12:40:52 +0300

    FirstCommit

--- Personality Quiz/ViewControllers/ViewController.swift
+++ Personality Quiz/ViewControllers/ViewController.swift
@@ -2,12 +2,14 @@
 //  ViewController.swift
 //  Personality Quiz
 //
-//  Created by Boris Alexandrov on 03.07.2022.
+//  Created by Bori Zitserman on 03.07.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let questions = Question.getQuestion()
 
     override func viewDidLoad() {
         super.viewDidLoad()
